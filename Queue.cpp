09210b40991568d215017392929bc1c8b5e7a7a8
lienputnina <liene.putnina20@gmail.com>
2024-05-20 13:12:22 +0300

    feat: implemented Queue constructor and destructor

--- Queue.cpp
+++ Queue.cpp
@@ -1,1 +1,33 @@
 // Queue class implementation file
+#include "Queue.h"
+#include <cstddef>
+#include <iostream>
+using namespace std;
+
+template <class Type> Queue<Type>::Queue(int queueSize) {
+  queueArray = new Type[queueSize];
+  // Add a more modern check, perhaps
+  if (queueArray == NULL) {
+    cout << "Not enough memory. Program is shutting down." << endl;
+  };
+
+  size = queueSize;
+  arrayIndex = 0;
+};
+
+template <class Type> Queue<Type>::~Queue() { delete[] queueArray; };
+
+template <class Type>
+void Queue<Type>::enqueue(Type x){
+    // some code
+};
+
+template <class Type>
+Type Queue<Type>::dequeue(){
+    // some code
+};
+
+template <class Type>
+void Queue<Type>::PrintQueue(){
+    // some code
+};
--- Queue.h
+++ Queue.h
@@ -1,1 +1,22 @@
-// Queue class header file
+#ifndef Queue_h
+#define Queue_h
+
+template <class Type>
+
+class Queue {
+
+private:
+  Type *queueArray;
+  int size;
+  int arrayIndex;
+
+public:
+  Queue(int size);
+  ~Queue();
+
+  void enqueue(Type x);
+  Type dequeue();
+  void PrintQueue();
+};
+
+#endif
