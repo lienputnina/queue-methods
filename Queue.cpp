613faf3c2fd470b3dc895f7178e3e1d9ec60d8ab
lienputnina <liene.putnina20@gmail.com>
2024-05-20 13:22:32 +0300

    chore: added preliminary comments for Queue constructor/destructor

--- Queue.cpp
+++ Queue.cpp
@@ -5,8 +5,14 @@
 using namespace std;
 
 template <class Type> Queue<Type>::Queue(int queueSize) {
-  queueArray = new Type[queueSize];
+
+  /*
+  Allocating memory for Queue class object. This object will be an array with
+  the size the user will provide.
+  */
+  queueArray = new Type[queueSize]{};
   // Add a more modern check, perhaps
+  // Adding a check to see, if the memory is full
   if (queueArray == NULL) {
     cout << "Not enough memory. Program is shutting down." << endl;
   };
@@ -15,7 +21,13 @@ template <class Type> Queue<Type>::Queue(int queueSize) {
   arrayIndex = 0;
 };
 
-template <class Type> Queue<Type>::~Queue() { delete[] queueArray; };
+template <class Type> Queue<Type>::~Queue() {
+  /*
+ Deallocating the previously reserved memory for the queueArray array
+ to prevent a memory leak.
+*/
+  delete[] queueArray;
+};
 
 template <class Type>
 void Queue<Type>::enqueue(Type x){
--- Queue.h
+++ Queue.h
@@ -1,11 +1,16 @@
 #ifndef Queue_h
 #define Queue_h
 
+// Declaring the template type to make this class a template class
 template <class Type>
 
 class Queue {
 
 private:
+  /*
+    Declaring a pointer to an object of the generic type "Type".
+    This will later be used as a pointer to an array
+  */
   Type *queueArray;
   int size;
   int arrayIndex;
@@ -15,7 +20,13 @@ public:
   ~Queue();
 
   void enqueue(Type x);
+
+  /*
+    Declaring a method with "Type" to use for removing elements from a "Type"
+    object.
+  */
   Type dequeue();
+
   void PrintQueue();
 };
 
