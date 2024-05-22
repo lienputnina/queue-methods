c1b857b5ed72eaf167e0466f211665563bef124e
lienputnina <liene.putnina20@gmail.com>
2024-05-22 11:54:31 +0300

    chore: tested Queue methods with floats and chars

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
   cout << "Trying to delete the first element of the IntegerQueue:" << endl;
   IntegerQueue.dequeue();
 
-  cout << "Adding elements to IntegerQueue:" << endl;
+  cout << "Adding elements to the IntegerQueue:" << endl;
   IntegerQueue.enqueue(1);
   IntegerQueue.enqueue(2);
   IntegerQueue.enqueue(3);
@@ -41,6 +41,70 @@ int main() {
   cout << "\n";
 
   cout << "--------------" << endl;
+  cout << "\n";
+
+  cout << "Trying to delete the first element of the FloatQueue:" << endl;
+  FloatQueue.dequeue();
+
+  cout << "Adding elements to the FloatQueue:" << endl;
+  FloatQueue.enqueue(1.11);
+  FloatQueue.enqueue(2.22);
+  FloatQueue.enqueue(3.33);
+  FloatQueue.enqueue(4.44);
+  FloatQueue.enqueue(5.55);
+
+  cout << "FloatQueue has the following elements:" << endl;
+  FloatQueue.PrintQueue();
+  cout << "\n";
+
+  cout << "Trying to add more elements to the FloatQueue:" << endl;
+  FloatQueue.enqueue(6);
+  cout << "\n";
+
+  cout << "FloatQueue has the following elements:" << endl;
+  FloatQueue.PrintQueue();
+  cout << "\n";
+
+  cout << "Deleting the first element of the FloatQueue:" << endl;
+  FloatQueue.dequeue();
+  FloatQueue.PrintQueue();
+  cout << "\n";
+
+  cout << "FloatQueue after deleting the first element:" << endl;
+  FloatQueue.PrintQueue();
+  cout << "\n";
+
+  cout << "--------------" << endl;
+
+  cout << "Trying to delete the first element of the CharacterQueue:" << endl;
+  CharacterQueue.dequeue();
+
+  cout << "Adding elements to the CharacterQueue:" << endl;
+  CharacterQueue.enqueue('a');
+  CharacterQueue.enqueue('b');
+  CharacterQueue.enqueue('c');
+  CharacterQueue.enqueue('d');
+  CharacterQueue.enqueue('e');
+
+  cout << "CharacterQueue has the following elements:" << endl;
+  CharacterQueue.PrintQueue();
+  cout << "\n";
+
+  cout << "Trying to add more elements to the CharacterQueue:" << endl;
+  CharacterQueue.enqueue('f');
+  cout << "\n";
+
+  cout << "CharacterQueue has the following elements:" << endl;
+  CharacterQueue.PrintQueue();
+  cout << "\n";
+
+  cout << "Deleting the first element of the CharacterQueue:" << endl;
+  CharacterQueue.dequeue();
+  CharacterQueue.PrintQueue();
+  cout << "\n";
+
+  cout << "CharacterQueue after deleting the first element:" << endl;
+  CharacterQueue.PrintQueue();
 
   return 0;
 }
